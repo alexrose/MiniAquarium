208e6f45842c4091e7ee8e4f99ddca73a5f0b8de
Alex Trandafir <alex.itcv@gmail.com>
2020-05-01 14:36:08 +0300

    Updated cron time format

--- fish-admin/app/Models/Temperature.php
+++ fish-admin/app/Models/Temperature.php
@@ -10,7 +10,7 @@ class Temperature extends Model
         'value'
     ];
 
-    public function getUpdatedAtAttribute($value)
+    public function getTimeAttribute($value)
     {
         $value = Carbon::parse($value);
         return $value->format('H:i');
