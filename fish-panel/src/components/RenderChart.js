4cbf2b0aae14be97dc36717f9fb6a7e0221db3da
Alex Trandafir <alex.itcv@gmail.com>
2020-05-02 14:39:49 +0300

    React user interface - custom charts tooltip

--- fish-panel/src/components/RenderChart.js
+++ fish-panel/src/components/RenderChart.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
+import {Alert} from "react-bootstrap";
 
 class RenderChart extends Component {
 
@@ -7,15 +8,28 @@ class RenderChart extends Component {
         let {name, date, data, xKey, yKey} = this.props;
         name = `${name}(${date})`;
 
+
+        function RenderChartsTooltip({payload, label, active}) {
+            if (active) {
+                return (
+                    <Alert key='info' variant='info'>
+                        <small><strong>{payload[0].value}°</strong> ({label})</small>
+                    </Alert>
+                );
+            }
+
+            return null;
+        }
+
         return (
             <ResponsiveContainer width={'100%'} height={300}>
-                <LineChart data={data} >
-                    <CartesianGrid />
-                    <XAxis dataKey={xKey} />
-                    <YAxis domain={[24, 25]} />
-                    <Tooltip />
-                    <Legend verticalAlign='top' height={36} />
-                    <Line name={name} type='monotone' dataKey={yKey} dot={false} />
+                <LineChart data={data}>
+                    <CartesianGrid/>
+                    <XAxis dataKey={xKey}/>
+                    <YAxis domain={[24, 25]}/>
+                    <Tooltip content={<RenderChartsTooltip/>} />
+                    <Legend verticalAlign='top' height={36}/>
+                    <Line name={name} type='monotone' dataKey={yKey} dot={false}/>
                 </LineChart>
             </ResponsiveContainer>
         )
