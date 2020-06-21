31dff3804747cd9ae173a0d1cf36ab48a400eceb
Alex Trandafir <alex.itcv@gmail.com>
2020-06-21 18:06:36 +0300

    Remove laravel api

--- fish-admin/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('cron/settings', 'CronSettingsController@run');
-Route::get('settings', 'CronSettingsController@data');
-Route::get('cron/temperatures', 'CronTemperaturesController@run');
-Route::get('temperatures/{date?}', 'CronTemperaturesController@data');
